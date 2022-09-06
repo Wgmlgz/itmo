3b8c0dd8922c3b5fcae7891ab2cd3f7faa1d76a4
Wgmlgz <wgmlgz.god@gmail.com>
2022-09-06 20:17:48 +0300

    lab 0 part 1

---
+++ lab0/src/contents.sh
@@ -0,0 +1,97 @@
+echo "Тип диеты Nullivore" > bronzong3
+
+echo "Развитые способности\nHydration" > gorebyss7
+
+echo "Способности Focus Vital Spirit Anger\nPoint" > mankey
+
+echo "satk=6 sdef=10 spd=8" > mandibuzz
+
+echo "Тип диеты\nHerbivore" > sawsbuck
+
+echo "Тип диеты Phototroph" > bayleef
+
+echo "weigth=121.3 height=59.0 atk=7\ndef=7" > ludicolo
+
+echo "Живет Cave Mountain" > tyranitar
+
+echo "Живет Cave\nMountain" > cranidos
+
+echo "Возможности Overland=4 Surface=1 Sky=12 Jump=3\nPower4=0 Intelligence=3" > braviary
+
+echo "Способности Lick lovely Kiss Powder\nSnow Doubleslap Ice Punch Heart Stamp Mean Look Fake Tears Wake-Up\nSlap Avalanche Body Slam Wring Out Perish Song Blizzard" > jynx
+
+echo "Живет\nForest Grassland" > bayleef
+
+echo "Возможности Overland=4 Surface=7\nUnderwater=7 Jump=1 Power=1 Intelligence=2\nGilled=0" > kabute
+
+echo "weigth=283.5 height=79.0 atk=10\ndef=13" > tangrowth
+
+echo "Тип покемона NORMAL NONE" > Loudred
+
+echo "Возможности\nOverland=4 Surface=2 Jump=2 Power=1 Intelligence=2\nThreaded=0" > sewaddle
+
+echo "Живет Forest Rainforest" > shiftry
+
+echo "Холы After\nYou Drain Punch Endeavor Fire Punch Gravity Helping Hand Ice Punch\nKnock Off Magic Coat Pain Split Role Play Signal Beam Skill Swap Sleep\nTalk Snatch Snore Superpower Thunderpunch Trick Wonder Room Zen\nHeadbutt" > reuniclus
+
+echo "Живет Cave Urban" > shuppet
+
+echo "Способности Growl Tackle\nQuick Attack Wing Attack Double Team Endeavor whirlwind Aerial Ace\nTake Down Agility Brave Bird Final Gambit" > starly
+
+echo "Способности\nPoisonpowder Stun Spore Leech Life Fury Cutter Spore Slash Growth Giga\nDrain Aromatherapy Rage Powder X-Scissor" > parasect
+
+echo "satk=11 sdef=10\nspd=6" > porygon2
+
+echo "Возможности Overland=8 Surface=3 Jump=2 Power=2\nIntelligence=4 Tracker=0" > growlithe
+
+echo "weigth=16.1 height=20.0 atk=6\ndef=9" > swadloon
+
+echo "Тип покемона GRASS DARK" > cacturne
+
+echo "Возможности\nOverland=8 Surface=4 Jump=2 Power=2 Intelligence=7 Aura=0\nTelekinetic=0 Telepath=0" > gardevoir
+
+echo "weigth=73.9 height=20.0 atk=6\ndef=6" > phanpy
+
+echo "satk=6 sdef=4 spd=5" > croagunk
+
+echo "Развитые способности\nQuick Feet" > jolteon
+
+echo "Тип диеты Omnivore" > chimchar
+
+echo "Живет Forest\nGrassland" > jumpluff3
+
+echo "satk=7 sdef=5 spd=4" > numel
+
+echo "Развитые способности\nSand Force" > hippowdon
+
+echo "Способности Supersonic Constrict Acid Toxic\nSpikes Bubblebean Wrap Barrier Water Pulse Poison Jab Screech Hex\nHydro Pump Sludge Wave Wring Out" > tentacruel
+
+echo "Способности Overgrow\nVenom Chlorophyll Stench" > vileplume
+
+echo "Ходы Bug Bitet Electroweb False\nSwipet Giga Drain Heal Bell Helping Hand Iron Defense Knock Off Magic Coat Seed Bomb Signal Beam Sleep Talk Snore Synthesis\nWorry\nSeed" > leavanny
+
+echo "Развитые способности Sheer Force" > krabby
+
+echo "Ходы Dark\nPulset‡ Detect!‡Dragon Pulse Drain Punch Dual Chop Focus Punch Fury\nCutter Helping Hand Ice Punch Iron Defense Iron Tail Low Kick Magic\nCoat Magnet Rise Metal Claw‡ Mud-Slap Role Play Sleep Talk Snore Swift\nThunderpunch Vacuum Wave Wave Pulse Zen\nHeadbutt" > lucario
+
+echo "weigth=203.9 height=75.0 atk=10\ndef=7" > sawsbuck
+
+echo "Способности Astonish Defense Curl Pound Mud-Slap\nRollout Shadow Punch Iron Defense Mega Punch Magnitude Dynamicpunch\nNight Shade Curse Earthquake Hammer Arm Focus\nPunch" > gelett
+
+echo "Способности Blaze Focus Flash Fire Inner\nFocus" > nonferno
+
+echo "Способности Overcharge Static\nSoundproof" > electrode
+
+echo "Ходы Bounce Body Slam Counter Covet Double\nKick‡ Double-Edge Dynamicpunch Focus Punch Helping Hand Knock Off Low\nKick Magic Coat Mega Punch Metronome Hud-Slap Revenge Role Play\nSeismic Toss Sleep Talk Snore Sucker Punch Superpower Swift Vacuum\nWave" > hitmonlee
+
+echo "Способности Mud-Slap Headbutt Metal Claw Iron Defense\nRoar Take Down Iron Head Protect Metal Sound Iron Tail Autotomize\nHeavy Slam Double-Edge Metal Burst" > aggron
+
+echo "Живет Beach Taiga\nTundra" > piplup
+
+echo "weigth=27.6 height=24.0 atk=8\ndef=6" > timburr
+
+echo "Способности Focus Guts No Guard" > machop
+
+echo "Ходы\nCounter Destiny Bond Mirror Coat Safeguard" > wobbuffet
+
+echo "Тип покемона\nELECTRIC NONE" > shinx
--- lab0/src/script.sh
+++ lab0/src/script.sh
@@ -1,1 +1,116 @@
-echo hi
+
+# go to test dir
+cd ..
+rm -rf test
+mkdir test && cd $_
+
+
+# Part 1
+echo $'Тип диеты Nullivore' > bronzong3
+echo $'Развитые способности\nHydration' > gorebyss7
+mkdir houndoom1 && cd $_
+  mkdir conkeldurr && cd $_
+    echo $'Способности Focus Vital Spirit Anger\nPoint' > mankey
+    echo $'satk=6 sdef=10 spd=8' > mandibuzz
+    mkdir honchkrow && cd $_
+      mkdir scyther
+      echo $'Тип диеты\nHerbivore' > sawsbuck
+      mkdir dewgong
+      echo $'Тип диеты Phototroph\nHerbivore' > bayleef
+      mkdir turtwig
+      echo $'weigth=121.3 height=59.0 atk=7\ndef=7' > ludicolo
+      mkdir pansear && cd ..
+    mkdir machoke && cd $_
+      echo $'Живет Cave Mountain' > tyranitar
+      echo $'Живет Cave\nMountain' > cranidos && cd ..
+    echo $'Возможности Overland=4 Surface=1 Sky=12 Jump=3\nPower4=0 Intelligence=3' > braviary
+    echo $'Способности Lick lovely Kiss Powder\nSnow Doubleslap Ice Punch Heart Stamp Mean Look Fake Tears Wake-Up\nSlap Avalanche Body Slam Wring Out Perish Song Blizzard' > jynx && cd ..
+  mkdir flygon && cd $_
+    echo $'Живет\nForest Grassland' > bayleef
+    mkdir venipede && cd $_
+      mkdir larvesta
+      echo $'Возможности Overland=4 Surface=7\nUnderwater=7 Jump=1 Power=1 Intelligence=2\nGilled=0' > kabute
+      mkdir zweilous
+      echo $'weigth=283.5 height=79.0 atk=10\ndef=13' > tangrowth
+      echo $'Тип покемона NORMAL NONE' > Loudred
+      mkdir rhydon
+      mkdir wailord && cd ..
+    echo $'Возможности\nOverland=4 Surface=2 Jump=2 Power=1 Intelligence=2\nThreaded=0' > sewaddle 
+    mkdir lubicolo && cd $_
+      echo $'Живет Forest Rainforest' > shiftry
+      mkdir cradily
+      mkdir electivire && cd ..
+      cd ..
+  echo $'Холы After\nYou Drain Punch Endeavor Fire Punch Gravity Helping Hand Ice Punch\nKnock Off Magic Coat Pain Split Role Play Signal Beam Skill Swap Sleep\nTalk Snatch Snore Superpower Thunderpunch Trick Wonder Room Zen\nHeadbutt' > reuniclus
+  mkdir trapinch && cd $_
+    echo $'Живет Cave Urban' > shuppet
+    mkdir slaking && cd $_
+      mkdir beautifly
+      echo $'Способности Growl Tackle\nQuick Attack Wing Attack Double Team Endeavor whirlwind Aerial Ace\nTake Down Agility Brave Bird Final Gambit' > starly
+      echo $'Способности\nPoisonpowder Stun Spore Leech Life Fury Cutter Spore Slash Growth Giga\nDrain Aromatherapy Rage Powder X-Scissor' > parasect
+      mkdir sharpedo
+      mkdir lampent && cd ..
+    mkdir nincada && cd $_
+      echo $'satk=11 sdef=10\nspd=6' > porygon2
+      echo $'Возможности Overland=8 Surface=3 Jump=2 Power=2\nIntelligence=4 Tracker=0' > growlithe
+      echo $'weigth=16.1 height=20.0 atk=6\ndef=9' > swadloon
+      mkdir spoink
+      mkdir chingling
+      mkdir crobat
+      echo $'Тип покемона GRASS DARK' > cacturne && cd ..
+    mkdir yanmega && cd $_
+      echo $'Возможности\nOverland=8 Surface=4 Jump=2 Power=2 Intelligence=7 Aura=0\nTelekinetic=0 Telepath=0' > gardevoir
+      echo $'weigth=73.9 height=20.0 atk=6\ndef=6' > phanpy && cd ..
+    mkdir tentacool && cd $_
+      echo $'satk=6 sdef=4 spd=5' > croagunk
+      mkdir tranquill && cd ..
+    echo $'Развитые способности\nQuick Feet' > jolteon && cd ..
+  echo $'Тип диеты Omnivore' > chimchar && cd ..
+echo $'Живет Forest\nGrassland' > jumpluff3
+mkdir tyranitar6 && cd $_
+  echo $'satk=7 sdef=5 spd=4' > numel
+  echo $'Развитые способности\nSand Force' > hippowdon
+  echo $'Способности Supersonic Constrict Acid Toxic\nSpikes Bubblebean Wrap Barrier Water Pulse Poison Jab Screech Hex\nHydro Pump Sludge Wave Wring Out' > tentacruel && cd ..
+mkdir tyrogue4 && cd $_
+  mkdir blissey && cd $_
+    mkdir herdier && cd $_
+      mkdir sharpedo
+      echo $'Способности Overgrow\nVenom Chlorophyll Stench' > vileplume
+      mkdir masquerain
+      mkdir electrode
+      mkdir grimer
+      mkdir foongus && cd ..
+    echo $'Ходы Bug Bitet Electroweb False\nSwipet Giga Drain Heal Bell Helping Hand Iron Defense Knock Off Magic Coat Seed Bomb Signal Beam Sleep Talk Snore Synthesis\nWorry\nSeed' > leavanny
+    echo $'Развитые способности Sheer Force' > krabby && cd ..
+  echo $'Ходы Dark\nPulset‡ Detect!‡Dragon Pulse Drain Punch Dual Chop Focus Punch Fury\nCutter Helping Hand Ice Punch Iron Defense Iron Tail Low Kick Magic\nCoat Magnet Rise Metal Claw‡ Mud-Slap Role Play Sleep Talk Snore Swift\nThunderpunch Vacuum Wave Wave Pulse Zen\nHeadbutt' > lucario
+  mkdir sentret && cd $_
+    mkdir golduck && cd $_
+      echo $'weigth=203.9 height=75.0 atk=10\ndef=7' > sawsbuck
+      mkdir honchkrow
+      mkdir galvantula
+      mkdir stunky
+      echo $'Способности Astonish Defense Curl Pound Mud-Slap\nRollout Shadow Punch Iron Defense Mega Punch Magnitude Dynamicpunch\nNight Shade Curse Earthquake Hammer Arm Focus\nPunch' > golett
+      echo $'Способности Blaze Focus Flash Fire Inner\nFocus' > monferno
+      mkdir gliscor && cd ..
+    mkdir mandibuzz && cd $_
+      mkdir hitmonchan
+      mkdir elekid
+      echo $'Способности Overcharge Static\nSoundproof' > electrode && cd ..
+    echo $'Ходы Bounce Body Slam Counter Covet Double\nKick‡ Double-Edge Dynamicpunch Focus Punch Helping Hand Knock Off Low\nKick Magic Coat Mega Punch Metronome Hud-Slap Revenge Role Play\nSeismic Toss Sleep Talk Snore Sucker Punch Superpower Swift Vacuum\nWave' > hitmonlee
+    echo $'Способности Mud-Slap Headbutt Metal Claw Iron Defense\nRoar Take Down Iron Head Protect Metal Sound Iron Tail Autotomize\nHeavy Slam Double-Edge Metal Burst' > aggron
+    echo $'Живет Beach Taiga\nTundra' > piplup
+    mkdir rattata && cd $_
+      echo $'weigth=27.6 height=24.0 atk=8\ndef=6' > timburr
+      echo $'Способности Focus Guts No Guard' > machop
+      echo $'Ходы\nCounter Destiny Bond Mirror Coat Safeguard' > wobbuffet
+      echo $'Тип покемона\nELECTRIC NONE' > shinx && cd ..
+    mkdir floatzel && cd $_
+      mkdir flaaffy
+      mkdir metagross && cd ..
+    cd ..
+  cd ..
+
+#TODO Part 2
+#TODO Part 3
+#TODO Part 4
+#TODO Part 5
