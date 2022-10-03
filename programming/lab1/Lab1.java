548971181fb7136d82e5b5592c8ec473e23792ac
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2022-10-03 20:51:13 +0300

    inf lab 1 fix

--- programming/lab1/Lab1.java
+++ programming/lab1/Lab1.java
@@ -1,32 +1,55 @@
 import java.util.Random;
 
 class Lab1 {
-  public static void main(String[] args) {
-    short[] a = new short[8];
+  short[] a;
+  double[] x;
+  double[][] b;
+
+  static double f1(double x) {
+    return Math.log(Math.abs(Math.log(Math.pow(Math.tan(x), 2))));
+  }
+
+  static double f2(double x) {
+    return Math.pow(
+        (Math.sin(Math.atan((x + 0.5) / 25.0))) /
+            (Math.cbrt(Math.pow(x, x)) - 1),
+        Math.sin(Math.pow(x, x * (x + 2.0 / 3.0))));
+  }
+
+  static double f3(double x) {
+    return Math.sin(Math.asin(Math.cos(Math.tan(Math.cos(x)))));
+  }
+
+  public Lab1() {
+    a = new short[8];
     for (short t = 4, idx = 0; t <= 18; t += 2, ++idx)
       a[idx] = t;
 
-    double[] x = new Random().doubles(19, -12, 13).toArray();
+    x = new Random().doubles(19, -12, 13).toArray();
 
-    double[][] b = new double[a.length][x.length];
+    b = new double[a.length][x.length];
     for (int i = 0; i < a.length; ++i) {
       for (int j = 0; j < x.length; ++j) {
         if (a[i] == 16)
-          b[i][j] = Math.log(Math.abs(Math.log(Math.pow(Math.tan(x[j]), 2))));
+          b[i][j] = f1(x[j]);
         else if (a[i] == 10 || a[i] == 12 || a[i] == 14 || a[i] == 18)
-          b[i][j] = Math.pow(
-              (Math.sin(Math.atan((x[j] + 0.5) / 25.0))) /
-                  (Math.cbrt(Math.pow(x[j], x[j])) - 1),
-              Math.sin(Math.pow(x[j], x[j] * (x[j] + 2.0 / 3.0))));
+          b[i][j] = f2(x[j]);
         else
-          b[i][j] = Math.sin(Math.asin(Math.cos(Math.tan(Math.cos(x[j])))));
+          b[i][j] = f3(x[j]);
       }
     }
+  }
 
-    for (int i = 0; i < a.length; ++i) {
-      for (int j = 0; j < b.length; ++j)
-        System.out.format("%9.5f ", b[i][j]);
+  void print() {
+    for (int i = 0; i < b.length; ++i) {
+      for (int j = 0; j < b[i].length; ++j)
+        System.out.format("%10.5f ", b[i][j]);
       System.out.println();
     }
+  }
+
+  public static void main(String[] args) {
+    var lab1 = new Lab1();
+    lab1.print();
   }
 }
